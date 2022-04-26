20988ab2368e1b26269bb245e519a1f6cd755271
SturmEnte <sturm.ente@ctemplar.com>
2022-04-26 21:12:50 +0200

    Added default config and added a fuction that prints the config

--- server/src/main.js
+++ server/src/main.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require("express");
+
+const printConfig = require("./util/printConfig");
+
+const info = require("../json/info.json");
+const config = require("../json/config.json");
+
+console.log("Running duck-hub-server version " + info.version);
+printConfig(config);
+
+const app = express();
+
+app.listen(config.port);
---
+++ server/src/util/printConfig.js
@@ -0,0 +1,5 @@
+module.exports = (config) => {
+	console.log("Config:");
+	console.log("Port: ", config.port);
+	console.log("Web interface: ", config["web-interface"]);
+};
