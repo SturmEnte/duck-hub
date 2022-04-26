1f7de18dff0a3f1c9ef023e8711dca446c3b441f
SturmEnte <sturm.ente@ctemplar.com>
2022-04-26 21:31:22 +0200

    Added a landingpage

--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const printConfig = require("./util/printConfig");
 
@@ -9,5 +10,12 @@ console.log("Running duck-hub-server version " + info.version);
 printConfig(config);
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+	res.render("app", { version: info.version });
+});
 
 app.listen(config.port);
