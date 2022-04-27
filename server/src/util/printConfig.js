267b4f1a89dbed4f950d5bbbcc0ddfa8398368be
SturmEnte <sturm.ente@ctemplar.com>
2022-04-27 23:45:39 +0200

    Replaced - with _ in the web-interface var

--- server/src/util/printConfig.js
+++ server/src/util/printConfig.js
@@ -1,6 +1,6 @@
 module.exports = (config) => {
 	console.log("Config:");
 	console.log("Port: ", config.port);
-	console.log("Web interface: ", config["web-interface"]);
+	console.log("Web interface: ", config.web_interface);
 	console.log("MongoDB Uri: ", config.mongodb_uri);
 };
