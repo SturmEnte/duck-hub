90ca12b57eb5e5cb597041b62ee3fd321a8b3363
SturmEnte <sturmente@proton.me>
2022-05-18 19:09:05 +0200

    Added basic data sync

--- server/src/public/js/db.js
+++ server/src/public/js/db.js
@@ -1,22 +1,16 @@
 const db = {};
 const data = new Map();
 
-const LOCAL_DB_CACHE_NAME = "local-db";
+const LOCAL_DB_CACHE_NAME = "local_db";
 
-db.set = (key, value) => {
+db.set = async (key, value) => {
 	data.set(key, value);
 	db.saveLocal();
-	fetch("/api/data/setUserData", {
-		method: "post",
-		headers: {
-			Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
-			"content-type": "application/json",
-		},
-		body: JSON.stringify({
-			key,
-			value,
-		}),
-	});
+	if ((await db.setOnline(key, value)) === 1) {
+		let iId = setInterval(async () => {
+			if ((await db.setOnline(key, value)) === 0) clearInterval(iId);
+		}, 10000);
+	}
 };
 
 db.get = (key) => {
@@ -53,7 +47,8 @@ db.load = async () => {
 
 	newData = false;
 	resData.forEach((value) => {
-		if (data.has(value.key)) {
+		if (!data.has(value.key)) {
+			if (data.get(value.key) == value.value) return;
 			data.set(value.key, value.value);
 			newData = true;
 		}
@@ -62,6 +57,45 @@ db.load = async () => {
 	if (newData === true) {
 		db.saveLocal();
 	}
+
+	// Override the online data with the local data if the local data is newer than the online data
+	if (newData === false) {
+		let done = [];
+		resData.forEach((value) => {
+			if (data.get(value.key) == value.value) return;
+			done.push(value.key);
+			db.setOnline(value.key, data.get(value.key));
+			console.log("Found local var thats newer than the offline one: ", value.key);
+		});
+
+		// Set all variables that only exist offline
+		data.forEach((value, key) => {
+			console.log("Data ", key);
+			if (done.includes(key)) return;
+			db.setOnline(key, value);
+			console.log("Found var thats only offline: ", key);
+		});
+	}
+};
+
+db.setOnline = async (key, value) => {
+	try {
+		await fetch("/api/data/setUserData", {
+			method: "post",
+			headers: {
+				Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+				"content-type": "application/json",
+			},
+			body: JSON.stringify({
+				key,
+				value,
+			}),
+		});
+	} catch (error) {
+		console.log(error);
+		return 1;
+	}
+	return 0;
 };
 
 db.load();
