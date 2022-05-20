2d2693366c0583a341948020e1781bfdcc71a5d2
SturmEnte <sturmente@proton.me>
2022-05-20 21:09:47 +0200

    Added a browser check to the token manager
    Because Firefox doesn't support hi-base64. I also added a alert-message that tells the user that Firefox may not support all features. I will implement better Firefox support later.

--- server/src/public/ts/manager/TokenManager.ts
+++ server/src/public/ts/manager/TokenManager.ts
@@ -19,7 +19,22 @@ export default class TokenManager {
 			console.log("Logout");
 		}
 
-		const tokenData = JSON.parse(base64.decode(this.refreshToken.split(".")[1]));
+		let tokenData;
+
+		// Firefox doesn't support hi-base64 because of that it needs to use the already included base64 decoder
+		if (navigator.userAgent.includes("Firefox")) {
+			// Displaying a notice that tells the user, that maybe not all features work in Firefox
+			if (!localStorage.getItem("ff-notice-done")) {
+				alert(
+					"Firefox detected! Some features may not work in Firefox. If you encounter any problems, please get in contact with a developer of Duck Hub. You can get in contact with me, by creating a issue describing your problem in the Duck Hub repository (https://github.com/sturmente/duck-hub)\n\nThis message will not appear again!"
+				);
+				localStorage.setItem("ff-notice-done", "y");
+			}
+
+			tokenData = JSON.parse(atob(this.refreshToken.split(".")[1]));
+		} else {
+			tokenData = JSON.parse(base64.decode(this.refreshToken.split(".")[1]));
+		}
 
 		this.userData = {
 			id: tokenData.id,
