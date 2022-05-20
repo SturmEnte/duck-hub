06b589c436096a6de527041555b9de25ee0e0d16
SturmEnte <sturmente@proton.me>
2022-05-20 23:33:36 +0200

    Moved the tokenData code into an function

--- server/src/public/ts/manager/TokenManager.ts
+++ server/src/public/ts/manager/TokenManager.ts
@@ -19,22 +19,7 @@ export default class TokenManager {
 			console.log("Logout");
 		}
 
-		let tokenData;
-
-		// Firefox doesn't support hi-base64 because of that it needs to use the already included base64 decoder
-		if (navigator.userAgent.includes("Firefox")) {
-			// Displaying a notice that tells the user, that maybe not all features work in Firefox
-			if (!localStorage.getItem("ff-notice-done")) {
-				alert(
-					"Firefox detected! Some features may not work in Firefox. If you encounter any problems, please get in contact with a developer of Duck Hub. You can get in contact with me, by creating a issue describing your problem in the Duck Hub repository (https://github.com/sturmente/duck-hub)\n\nThis message will not appear again!"
-				);
-				localStorage.setItem("ff-notice-done", "y");
-			}
-
-			tokenData = JSON.parse(atob(this.refreshToken.split(".")[1]));
-		} else {
-			tokenData = JSON.parse(base64.decode(this.refreshToken.split(".")[1]));
-		}
+		const tokenData = this.getTokenData(this.refreshToken);
 
 		this.userData = {
 			id: tokenData.id,
@@ -55,5 +40,22 @@ export default class TokenManager {
 
 	public getTokenUserData(): TokenUserData {
 		return this.userData;
+	}
+
+	private getTokenData(token: string): any {
+		// Firefox doesn't support hi-base64 because of that it needs to use the already included base64 decoder
+		if (navigator.userAgent.includes("Firefox")) {
+			// Displaying a notice that tells the user, that maybe not all features work in Firefox
+			if (!localStorage.getItem("ff-notice-done")) {
+				alert(
+					"Firefox detected! Some features may not work in Firefox. If you encounter any problems, please get in contact with a developer of Duck Hub. You can get in contact with me, by creating a issue describing your problem in the Duck Hub repository (https://github.com/sturmente/duck-hub)\n\nThis message will not appear again!"
+				);
+				localStorage.setItem("ff-notice-done", "y");
+			}
+
+			return JSON.parse(atob(this.refreshToken.split(".")[1]));
+		} else {
+			return JSON.parse(base64.decode(this.refreshToken.split(".")[1]));
+		}
 	}
 }
